0bb927147827e1a7331ccff6022c0f4849b125c1
Dominic <dominic.pr93@gmail.com>
2021-01-31 22:42:45 +0100

    bootstrapped with create-next-app and added netlifycms

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { attributes, react as HomeContent } from "../content/home.md";
+
+export default function Home() {
+	let { title, cats } = attributes;
+	console.log(attributes);
+
+	return (
+		<div>
+			<h1>title</h1>
+			{cats.map((cat, index) => {
+				return (
+					<div key={index}>
+						<h3>{cat.name}</h3>
+						<p>{cat.description}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
